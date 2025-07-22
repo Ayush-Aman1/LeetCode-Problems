ba20f3af04cbf2eecee7aa027e2ec6d46123a72e
Ayush Aman <146347360+Ayush-Aman1@users.noreply.github.com>
2025-07-22 13:39:07 +0530

    Add 26_remove_duplicates_from_sorted_array.cpp - LeetCode Problem 26

---
+++ 26_remove_duplicates_from_sorted_array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.empty()) return 0;
+
+        int k = 1; // Position to write next unique value
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+};
