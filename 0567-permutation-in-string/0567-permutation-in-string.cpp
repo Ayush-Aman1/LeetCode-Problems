283d463a68adde8229c61caa0ecbd0d8055529fe
Ayush Aman <146347360+Ayush-Aman1@users.noreply.github.com>
2025-08-18 11:08:00 +0530

    Time: 569 ms (8.5%), Space: 135.3 MB (5.01%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -3,21 +3,27 @@ public:
     bool checkInclusion(string s1, string s2) {
         int k = s1.size();
         int n = s2.size();
-        string subs;
-        string perm = s1;
-        sort(perm.begin(), perm.end());
         int l = 0, r = k-1;
+        unordered_map<char, int> count1;
+        for (char c : s1) {
+            count1[c]++;
+        }
+        unordered_map<char, int> count2;
         while(r<n){
+            unordered_map<char, int> count2;
+            string subs;
             subs = s2.substr(l, k);
-            sort(subs.begin(), subs.end());
-            if(perm != subs){
+            for(char c:subs){
+                count2[c]++;
+            }
+            if(count1 == count2){
+                return true;
+            }
+            else{
                 l++;
                 r++;
             }
-            else{
-                break;
-            }
         }
-        return (subs == perm);
+        return false;
     }
 };
