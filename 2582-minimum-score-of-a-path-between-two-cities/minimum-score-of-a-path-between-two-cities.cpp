46b4fb92ae7b3105fbe0f7d99d0c9a37477e30a6
Ayush Aman <146347360+Ayush-Aman1@users.noreply.github.com>
2026-07-04 17:58:30 +0530

    Time: 65 ms (51.55%) | Memory: 136.3 MB (46.28%) - LeetSync

---
+++ 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minScore(int n, vector<vector<int>>& roads) {
+        vector<vector<pair<int, int>>> g(n + 1);
+        for (auto &r : roads) {
+            g[r[0]].push_back({r[1], r[2]});
+            g[r[1]].push_back({r[0], r[2]});
+        }
+        vector<int> vis(n + 1);
+        int ans = INT_MAX;
+        function<void(int)> dfs = [&](int u) {
+            vis[u] = 1;
+            for (auto &[v, d] : g[u]) {
+                ans = min(ans, d);
+                if (!vis[v]) dfs(v);
+            }
+        };
+        dfs(1);
+        return ans;
+    }
+};
